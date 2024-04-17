24e6aad186208d213573978a3da4e47bfb8f7b67
gabriel adelar <gabriel_adelar@hotmail.com>
2024-04-17 13:56:39 -0300

    teste

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,25 @@ export default defineNuxtConfig({
     '@/assets/css/reset.css',
     '@/assets/css/global.css',
   ],
-  app:{
-    baseURL: '/portifolio/'
-  }
+  router: {
+    base: '/portifolio/'
+  },
+  head: {
+    title: 'Portifolio',
+    
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+
+      // header security
+      {name:'X-Content-Type-Options', content:'nosniff'},
+      {name:'X-Frame-Options', content:'DENY'},
+      {name:'X-XSS-Protection', content:'1; mode=block'},
+      {name:'Referrer-Policy', content:'strict-origin-when-cross-origin'},
+      {name:'Feature-Policy', content:"geolocations, 'self'"}
+    ],
+    
+  },
 })
